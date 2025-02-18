cc9d3a2b6fdc98c66778db57d122648a31532940
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-02-18 20:00:10 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/springboot3lab420251/Springboot3lab420251Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot3lab420251;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3lab420251Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3lab420251Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springboot3lab420251/Springboot3lab420251ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot3lab420251;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3lab420251ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
